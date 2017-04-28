7dc2e5470f9c46763629fd207a3d07af2a4987c6
James Jarvis <jamespjarvis@gmail.com>
2017-04-28 11:32:48 -0400

    added video-list

--- public/app.js
+++ public/app.js
@@ -10,55 +10,87 @@ const volumeRange = player.querySelector(`input[name='volume']`);
 const skipButtons = player.querySelectorAll('button[data-skip]');
 const ranges = player.querySelectorAll(`input[type='range']`);
 
+const videoListItems = document.querySelectorAll('.list-item');
+
+videoListItems.forEach(item => item.addEventListener('click', handleListClick));
+
+// update video player source
+function handleListClick(e) {
+  // update source
+  video.src = `/videos/video${this.dataset.video}.mp4`;
+  video.currentTime = 0;
+  video.play();
+
+  // add active class to current video
+  videoListItems.forEach(item => {
+    if (item.classList.contains('active')) {
+      item.classList.remove('active');
+    }
+  });
+  this.classList.add('active');
+}
+
+// toggle video play state
 function togglePlay() {
-    video[video.paused ? 'play' : 'pause']();
+  video[video.paused ? 'play' : 'pause']();
 }
 
+// toggle fullscreen video player
 function toggleFullscreen(e) {
-    if (document.webkitFullscreenElement) {
-        document.webkitExitFullscreen();
-    } else {
-        player.webkitRequestFullscreen();
-    }
+  if (document.webkitFullscreenElement) {
+    document.webkitExitFullscreen();
+    fullscreen.textContent = '';
+  } else {
+    player.webkitRequestFullscreen();
+    fullscreen.textContent = '✕';
+  }
 }
 
+// update volume
 function updateVolumeRange(level) {
-    volumeRange.value = level;
+  volumeRange.value = level;
 }
 
+// change volume icon based on level
+// TODO
 function toggleVolume() {
-    if (this.dataset.level === 'full') {
-        this.dataset.level = 'mute';
-        this.innerHTML = '&#128263;';
-        video.volume = 0;
-    } else {
-        this.dataset.level = 'full';
-        this.innerHTML = '&#128266;';
-        video.volume = 1;
-    }
-    updateVolumeRange(video.volume);
+  if (this.dataset.level === 'full') {
+    this.dataset.level = 'mute';
+    this.innerHTML = '&#128263;';
+    video.volume = 0;
+  } else {
+    this.dataset.level = 'full';
+    this.innerHTML = '&#128266;';
+    video.volume = 1;
+  }
+  updateVolumeRange(video.volume);
 }
 
+// update play / pause butotn based on video state
 function updateButton() {
-    toggle.textContent = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = this.paused ? '►' : '❚ ❚';
 }
 
+// skip forward / back
 function skip() {
-    video.currentTime += parseFloat(this.dataset.skip);
-    console.log('skip');
+  video.currentTime += parseFloat(this.dataset.skip);
+  console.log('skip');
 }
 
+// update volume and / or playback rate
 function handleRangeUpdate() {
-    video[this.name] = this.value;
+  video[this.name] = this.value;
 }
 
+// update ui progress bar based on current time and duration
 function handleProgressUpdate() {
-    const percent = (video.currentTime / video.duration) * 100;
-    progressBar.style.flexBasis = `${percent}%`;
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
 }
 
+// change play position
 function scrub(e) {
-    video.currentTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = (e.offsetX / progress.offsetWidth) * video.duration;
 }
 
 video.addEventListener('click', togglePlay);
